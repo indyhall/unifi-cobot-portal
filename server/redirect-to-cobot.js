071828af1c710725fe5f41109dd26ac2c048982c
Chris Morrell <chris@cmorrell.com>
2018-03-30 16:20:14 -0400

    WIP

--- server/redirect-to-cobot.js
+++
@@ -1,5 +0,0 @@
-module.exports = (req, res) => {
-	return res.json({
-		url: 'https://www.google.com/' // FIXME
-	});
-};
--- server/store-member.js
+++ server/store-member.js
@@ -1,3 +1,35 @@
+
+const fetch = require('node-fetch');
+const qs = require('qs');
+
 module.exports = (req, res) => {
-	return res.send('Working on it.');
+	
+	const { email, password } = req.body;
+	
+	const query = qs.stringify({
+		'scope': 'create_password_resets, read_custom_fields, read_memberships, and write_custom_fields',
+		'grant_type': 'password',
+		'username': email,
+		'password': password,
+		'client_id': process.env.COBOT_CLIENT_ID,
+		'client_secret': process.env.COBOT_CLIENT_SECRET
+	});
+	
+	const url = `https://www.cobot.me/oauth/access_token?${query}`;
+	
+	const opts = {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+		},
+	};
+	
+	fetch(url, opts)
+		.then(result => result.json())
+		.then(json => {
+			res.json({
+				req_url: url,
+				...json
+			});
+		});
 };
