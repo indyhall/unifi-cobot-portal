8e19e9654d9dfcc75888eef49192fce2ab49ad8c
Chris Morrell <chris@cmorrell.com>
2018-04-06 11:46:17 -0400

    WIP

--- server/guest.js
+++ server/guest.js
@@ -1,7 +1,7 @@
 const unifi = require('./unifi');
 
 module.exports = (req, res) => {
-	unifi.authorizeMac(req.body.mac, 1)
+	unifi.authorizeMac(req.body.mac, req.body.ap, 1) // FIXME: Authorize for just 1 minute
 		.then(result => res.json(result))
 		.catch(error => res.json({ error }));
 };
