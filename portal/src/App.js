cd7029cc30e1641fa7ddc3691d36f6eca8f3809a
Chris Morrell <chris@cmorrell.com>
2018-07-24 21:14:32 -0400

    Bugsnag

--- portal/src/App.js
+++ portal/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import qs from 'qs';
+import bugsnag from 'bugsnag-js';
+import createPlugin from 'bugsnag-react';
 import logo from "./logo.png";
 import Login from "./Login";
 
@@ -15,30 +17,35 @@ export default class App extends Component {
 			ssid, // SSID of connection
 		} = query;
 		
+		const bugsnagClient = bugsnag('7492120eeac8aa6ad2a56582a54d5889');
+		const ErrorBoundary = bugsnagClient.use(createPlugin(React));
+		
 		return (
-			<div className="mx-auto my-8 px-4" style={ { maxWidth: 500 } }>
-				<div>
-					<img
-						src={ logo }
-						width={ 150 }
-						className="block mx-auto"
-					/>
-				</div>
-				
-				<div className="my-4 p-2 bg-white border border-grey rounded shadow">
+			<ErrorBoundary>
+				<div className="mx-auto my-8 px-4" style={ { maxWidth: 500 } }>
+					<div>
+						<img
+							src={ logo }
+							width={ 150 }
+							className="block mx-auto"
+						/>
+					</div>
 					
-					{ id && <Login mac={ id } url={ url } ap={ ap } t={ t } ssid={ ssid } /> }
+					<div className="my-4 p-2 bg-white border border-grey rounded shadow">
+						
+						{ id && <Login mac={ id } url={ url } ap={ ap } t={ t } ssid={ ssid } /> }
+						
+						{ !id && (
+							<p className="text-grey-darker mx-2 my-4 leading-normal">
+								<strong>We don't recognize this device </strong> and no address
+								information appears to be available. Please make sure you are connected
+								to the correct network and try again.
+							</p>
+						) }
 					
-					{ !id && (
-						<p className="text-grey-darker mx-2 my-4 leading-normal">
-							<strong>We don't recognize this device </strong> and no address
-							information appears to be available. Please make sure you are connected
-							to the correct network and try again.
-						</p>
-					) }
-				
+					</div>
 				</div>
-			</div>
+			</ErrorBoundary>
 		);
 	}
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const morgan = require('morgan');
+const bugsnag = require('bugsnag');
 
 const env = require('./env');
 
+bugsnag.register('f2bba740a2b92df2bd6905e341792921');
+
 const app = express();
 
+app.use(bugsnag.requestHandler);
+app.use(bugsnag.errorHandler);
+
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '..', 'portal', 'build')));
 
